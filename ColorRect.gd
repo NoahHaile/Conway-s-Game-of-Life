9198ece173c66ed1cb2a741ec0275277f664bcde
Noah Hailegiorgis <noahhaile456@gmail.com>
2023-11-17 12:48:21 -0800

    Grid basics complete

--- ColorRect.gd
+++ ColorRect.gd
@@ -5,11 +5,15 @@ func _ready():
 	# Set the initial color of the ColorRect to black.
 	self.color = Color.DARK_GRAY
 	self.size = get_viewport_rect().size
-	for i in range(20): # This will create 4 children as an example.
-		var child_rect = Cell.new()
-		# Calculate a position offset for each child to avoid overlap.
-		child_rect.position = Vector2(i * 32, 0)
-		add_child(child_rect)
+	var size = 30
+	var gap = 2
+	for j in range(get_viewport_rect().size.x / size + gap ):
+		for i in range(get_viewport_rect().size.x / size + gap ): # This will create 4 children as an example.
+			var child_rect = Cell.new()
+			child_rect.size = Vector2(size, size) 
+			# Calculate a position offset for each child to avoid overlap.
+			child_rect.position = Vector2(i * ( size + gap ), j * ( size + gap ))
+			add_child(child_rect)
 	# Make sure the ColorRect is reacting to mouse input.
 
 # Override the _input function to respond to mouse clicks.
