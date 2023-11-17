212f63451e799ae4030ba15c0b68830f0abdc2a9
Noah Hailegiorgis <noahhaile456@gmail.com>
2023-11-17 12:01:10 -0800

    Color Switching added

---
+++ ColorRect.gd
@@ -0,0 +1,20 @@
+extends ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Set the initial color of the ColorRect to black.
+	self.color = Color.BLACK
+	# Make sure the ColorRect is reacting to mouse input.
+	set_process_input(true)
+
+# Override the _input function to respond to mouse clicks.
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		# Calculate the local position by subtracting the global position of the ColorRect.
+		var local_position = event.position - global_position
+		# Check if the click is within the ColorRect area.
+		if get_rect().has_point(local_position):
+			# If the color is black, change it to white, and vice versa.
+			self.color = Color.WHITE if self.color == Color.BLACK else Color.BLACK
+
+# _process function can be removed if not used for other purposes.
