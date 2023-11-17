6df7a9f628b0683372fd418446427b9e72dc368e
Noah Hailegiorgis <noahhaile456@gmail.com>
2023-11-17 13:19:17 -0800

    Branching of to testing phase. Requires a different direction.

--- ColorRect.gd
+++ ColorRect.gd
@@ -1,22 +1,41 @@
 extends ColorRect
 const Cell = preload("res://cell.gd")
 # Called when the node enters the scene tree for the first time.
+
+var size = 30
+var gap = 2
+var number_of_rows = get_viewport_rect().size.x / size + gap
+
 func _ready():
 	# Set the initial color of the ColorRect to black.
 	self.color = Color.DARK_GRAY
 	self.size = get_viewport_rect().size
-	var size = 30
-	var gap = 2
-	for j in range(get_viewport_rect().size.x / size + gap ):
-		for i in range(get_viewport_rect().size.x / size + gap ): # This will create 4 children as an example.
+	
+	for j in range( number_of_rows ):
+		for i in range( number_of_rows ):
 			var child_rect = Cell.new()
 			child_rect.size = Vector2(size, size) 
 			# Calculate a position offset for each child to avoid overlap.
 			child_rect.position = Vector2(i * ( size + gap ), j * ( size + gap ))
 			add_child(child_rect)
-	# Make sure the ColorRect is reacting to mouse input.
 
-# Override the _input function to respond to mouse clicks.
+func get_colorRect_life():
+	var cell_colors = []
+	for child in self.get_children():
+		if child is Cell:
+			cell_colors.append(child.color == Color.WHITE if true else false)
+			
+	return cell_colors
 
-
+func count_living_neighbors(index, cell_states):
+	var living_neighbors_count = 0
+	if( index - number_of_rows > 0 ):
+		
+		
+	
+func survival_check():
+	var cell_states = get_colorRect_life()
+	
 # _process function can be removed if not used for other purposes.
+func _process(delta):
+	pass
